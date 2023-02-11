3fee01998c4168a501dc1e430505260e76974884
Arghyadeep <105662359+K-Arghyadeep@users.noreply.github.com>
2023-02-11 12:00:26 +0530

    question3

---
+++ sem4/pca/python/pca1/positiveNegativeAverage.py
@@ -0,0 +1,18 @@
+#question3
+user_input = int(input("Enter -1 to exit..."))
+p_sum = 0
+p_count = 0
+n_sum = 0
+n_count = 0
+
+while(user_input != -1):
+    if(user_input > 0):
+        p_sum += user_input
+        p_count += 1
+    elif(user_input < 0):
+        n_sum += user_input
+        n_count += 1
+    user_input = int(input("Enter a number:"))
+
+print("The average of negative numbers is: ", (n_sum/n_count))
+print("The average of positve is: {:.9f}".format (p_sum/p_count))
